659eb78934abcb5b0d2c25a605ebfb7498e67750
Mariadelaverde98 <mariadelaverde98@gmail.com>
2022-09-20 12:52:04 +0200

    20/09/2022 v2

--- ejercicio4/script.js
+++ ejercicio4/script.js
@@ -1,19 +1,21 @@
 //Almacena en dos variables datos de validación (usuario y contraseña) correctos y permite que el usuario valide (dispone de 3 intentos)
 function validar() {
-    var user = "usuario";
-    var pass = "contraseña";
+    const user = "usuario";
+    const pass = "contraseña";
     var intentos = 0;
     var correcto = false;
+    var usuario;
+    var contraseña;
     do{
-        var usuario = window.prompt("Inserta tu usuario: ");
-        var contraseña = window.prompt("Inserta tu contraseña: ");
-        if (usuario == user && contraseña == pass) {
-            correcto = true;
+        usuario = window.prompt("Inserta tu usuario: ");
+        contraseña = window.prompt("Inserta tu contraseña: ");
+        correcto = usuario == user && contraseña == pass;
+        if (correcto) {
             alert('todo correcto');
         }else{
             intentos++;
             alert('usuario o contraseña incorrectos');
         }  
-    }while(correcto == false && intentos < 3)
+    }while(!correcto && intentos < 3)
 
 }
